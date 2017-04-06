c3c16a64b152c2e3f9ef22a62ff1a621b78927d3
Abdulrahman Y. idlbi <adlogi@MacBook-Pro.local>
2017-04-06 16:16:10 +0300

    changing default lang to ar

--- arduino_extension.js
+++ arduino_extension.js
@@ -548,7 +548,7 @@
   // Check for GET param 'lang'
   var paramString = window.location.search.replace(/^\?|\/$/g, '');
   var vars = paramString.split("&");
-  var lang = 'en';
+  var lang = 'ar';
   for (var i=0; i<vars.length; i++) {
     var pair = vars[i].split('=');
     if (pair.length > 1 && pair[0]=='lang')
